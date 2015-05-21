ef76441db40688779f8d63abe50640baf9ab4a96
kalorobi <kalo.robi@gmail.com>
2015-05-21 20:47:38 +0200

    Kommentelés.

--- downloadMap.py
+++ downloadMap.py
@@ -1,26 +1,42 @@
-import urllib, math, glob, os, sys, getopt
 from PIL import Image
+import math
+import urllib
 
+#--------------------------------------------
+# GPS pozicio.
+# lat: float tipusu
+# lon: float tipusu
+#--------------------------------------------
 class Position:
-    """GPS pozicio. lat lon"""
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
- 
+
+#--------------------------------------------
+# Tile (csempe) leirasara valo osztaly.
+# x,y: tile "pozicioja"
+# zoom: a terkepen a zoom szint.
+#--------------------------------------------
 class Tile:
-    """tile"""
     def __init__(self, x, y, zoom):
         self.x = x
         self.y = y
         self.zoom = zoom
-        
+
+#--------------------------------------------
+# Letoletendo terulet, melyet egyertelmuen
+# meghataroz a position1 es position2.
+# position1 es postion2: Position tipusu.
+# zoom: a tile meghatarozashoz szukseges.
+#
+# widthXY, heightXY: szamitott ertekek, a
+# letoltendo terulet szelessege es magassaga
+# csempe szamban.
+#
+# origoXY: a letoltendo terulet origoja, bal
+# also sarok, csempe pozicio.
+#--------------------------------------------
 class DownloadArea:
-    """Letoltendo terulet. 
-    position1 es position2 pontok altal meghatarozott teglalap
-    a letoltendo terulet.
-    zoom szukseges a tile meghatarozashoz
-    """
-        
     def __init__(self, position1, position2, zoom):
         position1XY = self.convertLatLon2XY(position1, zoom)
         position2XY = self.convertLatLon2XY(position2, zoom)
@@ -28,21 +44,23 @@ class DownloadArea:
         
         if position1XY.x > position2XY.x:
             x = position2XY.x
-            self.width = position1XY.x - x + 1
+            self.widthXY = position1XY.x - x + 1
         else:
             x = position1XY.x
-            self.width = position2XY.x - x + 1
+            self.widthXY = position2XY.x - x + 1
             
         if position1XY.y > position2XY.y:
             y = position1XY.y
-            self.height = position1XY.y - position2XY.y + 1
+            self.heightXY = position1XY.y - position2XY.y + 1
         else:
             y = position2XY.y
-            self.height = position2XY.y - position1XY.y + 1
+            self.heightXY = position2XY.y - position1XY.y + 1
             
-        self.origoXY = Tile(x,y,zoom)
-        
-    def convertLatLon2XY(self, position, zoom): #pozicio atszamitas WIKI szerint
+        self.origoXY = Tile(x, y, zoom)
+    #----------------------------------------
+    # poziciot szamit at tile poziciora. WIKI
+    #----------------------------------------
+    def convertLatLon2XY(self, position, zoom):
         """poziciot tile-re konvertalja """
         lat = math.radians(position.lat)
         n = 2.0 ** zoom
@@ -50,52 +68,65 @@ class DownloadArea:
         ytile = int((1.0 - math.log(math.tan(lat) + (1 / math.cos(lat))) / math.pi) / 2.0 * n)
         tile = Tile(xtile, ytile, zoom)
         return (tile)
- 
+
+#--------------------------------------------
+# Terkep letoltes. 
+# map: pl.:http://a.tile.openstreetmap.com/
+#
+# downloadTiles() osztaly vegzi a letoltest.
+# dir: string, csempe mentes helye
+# downloadArea: DownloadArea tipus, letoltendo
+#--------------------------------------------
 class DownloadMap:
-     def __init__(self, map):
-         self.map = map #pl.: http://a.tile.openstreetmap.com/
-         self.downloadError = False #letoltes kozbeni hiba
-         self.wrongTiles = [] #hibas csempek
+    def __init__(self, map):
+        self.map = map
+        self.downloadError = False #letoltes kozbeni hiba
+        self.wrongTiles = [] #hibas csempek
          
-     def downloadTiles(self, dir, downloadArea):
-         positionXY = Tile(downloadArea.origoXY.x, downloadArea.origoXY.y, downloadArea.origoXY.zoom)
-         print("download tiles: %s" %str(downloadArea.width * downloadArea.height))
-         
-         for x in range(0,downloadArea.width):
-             for y in range(0,downloadArea.height):
-                 url = self.map + str(positionXY.zoom) + '/' + str(positionXY.x) + '/' + str(positionXY.y) + ".png"
-                 file = dir + str(positionXY.zoom) + '_' + str(positionXY.x) + '_' + str(positionXY.y) + ".png"
-                 self.download(url, file) #tile letoltes
-                 positionXY.y = positionXY.y - 1
-             positionXY.x = positionXY.x + 1
-             positionXY.y = downloadArea.origoXY.y
-        
-         if self.downloadError != True: #hiba ellenorzes
-             print("Downloading completed!")
-             self.mapCreate(dir, downloadArea)
-         else:
-             print("---E R R O R---")
-             for wrong in self.wrongTiles:
-                print("wrong tile: %s" %wrong)
-             
-     def download(self, url, file):
-         try:
-             urllib.urlretrieve(url, file)
-         except:
-            self.downloadError = True
-            self.wrongTiles.append(url)        
-            
-     def mapCreate(self, dir, downloadArea):
+    def downloadTiles(self, dir, downloadArea):
         positionXY = Tile(downloadArea.origoXY.x, downloadArea.origoXY.y, downloadArea.origoXY.zoom)
-        mapWidth = downloadArea.width *  256 #terkep szelesseg pixelben
-        mapHeight = downloadArea.height * 256 #terkep magassag pixelben
+        print("download tiles: %s" % str(downloadArea.widthXY * downloadArea.heightXY))
+         
+        for x in range(0, downloadArea.widthXY):
+            for y in range(0, downloadArea.heightXY):
+                url = self.map + str(positionXY.zoom) + '/' + str(positionXY.x) + '/' + str(positionXY.y) + ".png"
+                file = dir + str(positionXY.zoom) + '_' + str(positionXY.x) + '_' + str(positionXY.y) + ".png"
+                self.download(url, file) #tile letoltes
+                positionXY.y = positionXY.y - 1
+            positionXY.x = positionXY.x + 1
+            positionXY.y = downloadArea.origoXY.y
+        
+        if self.downloadError != True: #hiba ellenorzes
+            print("Downloading completed!")
+            self.mapCreate(dir, downloadArea)
+        else:
+            print("---E R R O R---") #ide kell a hiba kezeles
+            for wrong in self.wrongTiles:
+                print("wrong tile: %s" % wrong)
+                
+    #----------------------------------------
+    # csempe letoltes es kozvetlen mentes
+    #----------------------------------------
+    def download(self, url, file):
+        try:
+            urllib.urlretrieve(url, file)
+            print("download:%s ->OK" %url)
+        except:
+            self.downloadError = True
+            self.wrongTiles.append(url)
+            print("download:%s ->ERROR" %url)
+            
+    def mapCreate(self, dir, downloadArea): #kulon osztalyba kell tenni
+        positionXY = Tile(downloadArea.origoXY.x, downloadArea.origoXY.y, downloadArea.origoXY.zoom)
+        mapWidth = downloadArea.widthXY * 256 #terkep szelesseg pixelben
+        mapHeight = downloadArea.heightXY * 256 #terkep magassag pixelben
         map = Image.new("RGB", (mapWidth, mapHeight), "white") #terkep
         
-        for x in range(0,downloadArea.width):
-            for y in range(0,downloadArea.height):
+        for x in range(0, downloadArea.widthXY):
+            for y in range(0, downloadArea.heightXY):
                 file = dir + str(positionXY.zoom) + '_' + str(positionXY.x) + '_' + str(positionXY.y) + ".png"
                 tile = Image.open(file)
-                map.paste(tile,(x * 256, mapHeight - (y * 256)-256))
+                map.paste(tile, (x * 256, mapHeight - (y * 256)-256))
                 positionXY.y = positionXY.y - 1
             positionXY.x = positionXY.x + 1
             positionXY.y = downloadArea.origoXY.y
--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ def main():
     dw = DownloadArea(poz1, poz2, zoom)
     
     print("X:%d Y:%d" %(dw.origoXY.x, dw.origoXY.y))
-    print("w:%d h:%d" %(dw.width, dw.height))
+    print("w:%d h:%d" %(dw.widthXY, dw.heightXY))
     map = "http://a.tile.openstreetmap.org/"
     dir = "/home/tile/"
     dt = DownloadMap(map)
